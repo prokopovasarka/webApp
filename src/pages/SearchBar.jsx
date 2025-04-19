86dec2a7478376967831b19e8ccffc377932a9bc
prokopovasarka <106741414+prokopovasarka@users.noreply.github.com>
2025-04-20 01:59:12 +0200

    Create SearchBar.jsx

---
+++ src/pages/SearchBar.jsx
@@ -0,0 +1,38 @@
+import { useState } from 'react';
+import axios from 'axios';
+
+const SearchBar = () => {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [searchInput, setSearchInput] = useState("");
+  const [data, setData] = useState([]);
+  
+  const fetchData = () => {
+    axios.get(https://s.livesport.services/api/v2/search?type-ids=${searchInput.type-ids})
+    .then( (response) => {
+      setData(response.data);
+      setLoading(true);
+    })
+    .catch( (error) => {
+      setError(error.message);
+      setLoading(false);
+    }, [])
+  };
+
+  const HandleChange = (e) => {
+    e.preventDefault();
+    setSearchInput(e.target.value);
+  };
+
+  return(
+    <div>
+      <input type="text" placeholder="Vyhledej" onChange={HandleChange} value={searchInput} />
+      <button className="search" onClick={fetchData}>Hledat</button>
+
+      {loading && <div>Načítám...</div>}
+      {error && <div>Chyba: {error}</div>}
+    </div>
+  );
+}
+
+export default SearchBar;
