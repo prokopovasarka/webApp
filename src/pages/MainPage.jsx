fd71b517bc54043bd44d6d7b31279b05372a3914
prokopovasarka <106741414+prokopovasarka@users.noreply.github.com>
2025-04-19 12:43:37 +0200

    Add files via upload

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,48 +1,68 @@
 import { Link } from 'react-router-dom';
 import { useState } from 'react';
+import axios from 'axios';
 import './MainPage.css'
 
 const MainPage = () => {
-    const MyButton = () => {
-        return(
-          <Link to = "/detail/">
-            <button>Detail</button>
-          </Link>
-        );
-    }
 
-    const SearchBar = () => {
-      const [SearchInput, SetSearchInput] = useState("");
-
-      const HandleChange = (e) => {
-        e.preventDefault();
-        SetSearchInput(e.target.value);
-      };
-
-      const HandleSearch = () => {
-        console.log("Vyhledávám:", SearchInput); // Zde by byl kód pro vyhledávání (např. filtrování nebo volání API)
-      };
-
+  const MyButton = () => {
       return(
-        <div>
-          <input type="text" placeholder="Vyhledej team" onChange={HandleChange} value={SearchInput} />
-          <button class="search" onClick={HandleSearch}>Hledat</button>
-        </div>
-
+        <Link to = "/detail/">
+          <button className="detailButton" >Detail</button>
+        </Link>
       );
-    }
+  }
 
-    
+  const SearchBar = () => {
+    const [searchInput, setSearchInput] = useState("");
+    const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+  
+    const FetchData = () => {
+      axios.get('https://s.livesport.services/api/v2/search?type-ids=${searchInput}')
+      .then( (response) => {
+        setData(response.data);
+        setLoading(false);
+      })
+      .catch( (error) => {
+        setError(error.message);
+        setLoading(true);
+      })
+    };
+
+    const HandleChange = (e) => {
+      e.preventDefault();
+      setSearchInput(e.target.value);
+    };
+
+    const HandleSearch = () => {
+      console.log("Vyhledávám:", searchInput);
+      FetchData();
+
+      if(loading) return <div>Loading...</div>;
+      if(error) return <alert>{error}</alert>;
+    };
+
+  
     return(
       <div>
-        <h1>Výsledky</h1>
-        <SearchBar/>
-        <div class="Teams">
-          <p>Zde bude tabulka</p>
-          <MyButton/>
-        </div>
+        <input type="text" placeholder="Vyhledej" onChange={HandleChange} value={searchInput} />
+        <button className="search" onClick={HandleSearch}>Hledat</button>
       </div>
     );
+  }
+  
+  return(
+    <div>
+      <h1>Výsledky</h1>
+      <SearchBar/>
+      <div className="Teams">
+        <p>Zde bude tabulka</p>
+        <MyButton/>
+      </div>
+    </div>
+  );
 }
 
 export default MainPage
